8d537e6fb6de5e41e5fb3a6d93f7824b722921ce
Andrea Baccega <vekexasia@gmail.com>
2026-05-12 10:05:43 +0200

    feat: add async safeRet support for ResultAsync & Promise<Result>
    - Add exported safeRet() function accepting PromiseLike<Result<T,E>>
    - Works with both ResultAsync and Promise<Result<T,E>>
    - Move module augmentation into src/index.ts (single types entry point)
    - Delete standalone types.d.ts
    - Fix repo URLs (neverthrow-saferet -> neverthrow-safe-ret)
    - Update description and keywords
    - Point types field to dist/index.d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import {Ok, Err} from 'neverthrow';
+import {Ok, Err, Result} from 'neverthrow';
+
+declare module "neverthrow" {
+  export interface Err<T, E> {
+    safeRet(): E extends never ? [E, T] : [E, undefined];
+  }
+
+  export interface Ok<T, E> {
+    safeRet(): T extends never ? [E, T] : [undefined, T];
+  }
+}
 
 Ok.prototype.safeRet = function () {
   return [undefined, this.value];
@@ -7,3 +17,22 @@ Ok.prototype.safeRet = function () {
 Err.prototype.safeRet = function () {
   return [this.error, undefined];
 };
+
+/**
+ * Async safeRet for PromiseLike<Result<T, E>>.
+ * Works with both ResultAsync and Promise<Result<T, E>>.
+ *
+ * @example
+ * ```ts
+ * // With ResultAsync
+ * const [e, v] = await safeRet(fetchUser(id));
+ * if (e) return err(e);
+ *
+ * // With Promise<Result>
+ * const [e, v] = await safeRet(myAsyncFn());
+ * if (e) return err(e);
+ * ```
+ */
+export function safeRet<T, E>(promise: PromiseLike<Result<T, E>>): Promise<[E, undefined] | [undefined, T]> {
+  return Promise.resolve(promise).then((res) => res.safeRet() as [E, undefined] | [undefined, T]);
+}
